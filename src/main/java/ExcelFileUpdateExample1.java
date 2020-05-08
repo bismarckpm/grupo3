07eecbd50a997f2996756db2ee0cf92665247e31
FuryAdcom <fastidiosowindows@hotmail.com>
2020-05-08 15:17:13 -0400

    Historia de Usuario 2

--- src/main/java/ExcelFileUpdateExample1.java
+++ src/main/java/ExcelFileUpdateExample1.java
@@ -2,6 +2,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+//Adrián: Agrego Iterator para in por cada columna y cada celda mostrando
+import java.util.Iterator;
+//\
 
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
@@ -40,6 +43,35 @@ public class ExcelFileUpdateExample1 {
 			int rowCount = sheet.getLastRowNum();
 
 			for (Object[] aBook : bookData) {
+				//Código Adrián para crear hojas nuevas al llegar a 30 celdas
+				if ( (rowCount) >= 30) {
+
+					sheet = workbook.getSheetAt(workbook.getNumberOfSheets()-1);
+					rowCount = sheet.getLastRowNum();
+					if (rowCount >= 30){
+						sheet = workbook.createSheet("Java Books " + (workbook.getNumberOfSheets()+1));
+
+						Object[][] startString = {
+							{"No", "Book Title", "Author", "Price"},
+						};
+						for (Object[] cells : startString) {
+							Row r = sheet.getRow(0);
+							r = sheet.createRow(0);
+	
+							int columnCount = 0;
+	
+							Cell c = r.createCell(columnCount);
+							for (Object field : cells) {
+								c = r.createCell(columnCount++);
+								c.setCellValue((String) field);
+							}
+						}
+					}
+
+					rowCount = sheet.getLastRowNum();
+				}
+				//\ Finaliza código
+
 				Row row = sheet.createRow(++rowCount);
 
 				int columnCount = 0;
@@ -57,6 +89,37 @@ public class ExcelFileUpdateExample1 {
 				}
 
 			}
+
+			//Código Adrián para mostrar contenido
+			for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
+				Iterator<Row> iterator = workbook.getSheetAt(i).iterator();
+
+				System.out.println("Hoja: " + workbook.getSheetAt(i).getSheetName());
+				while (iterator.hasNext()) {
+					Row nextRow = iterator.next();
+					Iterator<Cell> cellIterator = nextRow.cellIterator();
+					 
+					while (cellIterator.hasNext()) {
+						Cell cell = cellIterator.next();
+						 
+						switch (cell.getCellType()) {
+							case Cell.CELL_TYPE_STRING:
+								System.out.print(cell.getStringCellValue());
+								break;
+							case Cell.CELL_TYPE_BOOLEAN:
+								System.out.print(cell.getBooleanCellValue());
+								break;
+							case Cell.CELL_TYPE_NUMERIC:
+								System.out.print(cell.getNumericCellValue());
+								break;
+						}
+						if (cellIterator.hasNext())
+							System.out.print(" - ");
+					}
+					System.out.println();
+				}
+			}
+			//\ Finaliza código
 
 			inputStream.close();
 
